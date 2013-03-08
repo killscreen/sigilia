2bee79f5970848f741ced5a5ac224737a5b57c2c
killscreen <killscreen@users.noreply.github.com>
2013-03-07 20:22:07 -0800

    Refactor DecorationEngine to be non-specific to renderer

--- Sigilia/src/net/rowf/sigilia/renderer/decorator/Decorator.java
+++
@@ -1,8 +0,0 @@
-package net.rowf.sigilia.renderer.decorator;
-
-import net.rowf.sigilia.game.Entity;
-import net.rowf.sigilia.game.component.visual.Representation;
-
-public interface Decorator {
-	public Representation getRepresentationFor(Entity entity);
-}
