74801c54dada8aa7c3ce03fdd463d0293d19ef1a
killscreen <killscreen@users.noreply.github.com>
2013-03-20 23:17:37 -0700

    Added title screen; enforce landscape orientation

---
+++ Sigilia/src/net/rowf/sigilia/FullscreenActivity.java
@@ -0,0 +1,26 @@
+package net.rowf.sigilia;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Utility superclass for various game activities, so that they don't need to 
+ * individually manage common things (like refusing window title or constraining 
+ * orientation) 
+ * 
+ * @author woeltjen
+ *
+ */
+public abstract class FullscreenActivity extends Activity {
+	@Override
+	public void onCreate(Bundle b) {
+		super.onCreate(b);
+		
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		
+	}
+}
