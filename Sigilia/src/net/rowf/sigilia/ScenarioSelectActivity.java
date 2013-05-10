208b0a4bdc8fe9c4494301a8d68b5ec0375d8f2a
killscreen <killscreen@users.noreply.github.com>
2013-05-09 18:35:53 -0700

    Scenario select exposes new scenarios only after previous scenarios are completed (note: not retained beyond activity lifecycle)

--- Sigilia/src/net/rowf/sigilia/ScenarioSelectActivity.java
+++ Sigilia/src/net/rowf/sigilia/ScenarioSelectActivity.java
@@ -13,10 +13,12 @@ import android.view.View;
 
 
 
-public class ScenarioSelectActivity extends FullscreenActivity {
-	
+public class ScenarioSelectActivity extends FullscreenActivity {	
+	private int[] scenarios = { R.id.scenario_1, R.id.scenario_2, R.id.scenario_3 };
 	private Map<Integer, String> scenarioMap = new HashMap<Integer, String>();
-	private int activeScenario = 0;
+	private int activeScenario = 0;	
+	
+	private int highestScenarioAvailable = 0;
 	
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
@@ -30,16 +32,41 @@ public class ScenarioSelectActivity extends FullscreenActivity {
 		setContentView(R.layout.select);
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		// May include scenario success, etc.
+		for (int i = 0; i < scenarios.length; i++) {
+			View v = findViewById(scenarios[i]);
+			// TODO: Better visibility?
+			v.setVisibility(isAvailableToPlayer(scenarios[i]) ? View.VISIBLE : View.INVISIBLE);
+		}
+	}
+
+	private boolean isAvailableToPlayer(int scenarioId) {
+		for (int i = 0; i < scenarios.length; i++) {
+			if (i <= highestScenarioAvailable && 
+			    scenarios[i] == scenarioId) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
 	public void chooseScenario(View trigger) {
-		String scenario = scenarioMap.get(trigger.getId());
-		if (scenario != null) {
-			activeScenario = trigger.getId();
-			Intent runScenario = new Intent(this, ScenarioActivity.class);			
-			runScenario.putExtra(ScenarioActivity.SCENARIO_KEY, scenario);
-			startActivityForResult(runScenario, ScenarioActivity.SCENARIO_REQUEST);
-		} else {
-			Intent runScenario = new Intent(this, ECSDemoActivity.class);//ScenarioActivity.class);
-			startActivity(runScenario);
+		if (isAvailableToPlayer(trigger.getId())) {
+			String scenario = scenarioMap.get(trigger.getId());
+			if (scenario != null) {
+				activeScenario = trigger.getId();
+				Intent runScenario = new Intent(this, ScenarioActivity.class);			
+				runScenario.putExtra(ScenarioActivity.SCENARIO_KEY, scenario);
+				startActivityForResult(runScenario, ScenarioActivity.SCENARIO_REQUEST);
+			} else {
+				Intent runScenario = new Intent(this, ECSDemoActivity.class);//ScenarioActivity.class);
+				startActivity(runScenario);
+			}
 		}
 	}
 
@@ -48,6 +75,11 @@ public class ScenarioSelectActivity extends FullscreenActivity {
 		switch (resultCode) {
 		case ScenarioActivity.SCENARIO_SUCCESS:
 			Log.i(getClass().getSimpleName(), "Scenario success");
+			for (int i = 0; i < scenarios.length; i++) {
+				if (scenarios[i] == activeScenario) {
+					highestScenarioAvailable = i + 1;
+				}
+			}
 			break;
 		case ScenarioActivity.SCENARIO_FAILURE:
 			Intent hint = new Intent(this, HintActivity.class);
